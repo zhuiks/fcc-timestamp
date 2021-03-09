5f6d90bbba458df1648aef594e9c74dd3f944eb0
Evgen Kucherov <zhuke@ukr.net>
2021-03-09 21:10:14 +0000

    users Create/Read

--- api/exercise.js
+++ api/exercise.js
@@ -14,12 +14,36 @@ const exerciseSchema = new mongoose.Schema({
 });
 
 const exerciseLogSchema = new mongoose.Schema({
-  username: String,
+  username: {type: String, required: true, unique: true},
   log: [exerciseSchema],
 });
 
 const ExerciseLog = mongoose.model('ExerciseLog', exerciseLogSchema);
 
 
+app.post('/new-user', async (req, res) => {
+  try {
+    await connectDb();
+    const user = new ExerciseLog({username: req.body.username});
+    const result = await user.save()
+    console.log(result);
+    res.json({
+      _id: result._id,
+      username: result.username,
+    });
+  } catch(err) {
+    res.json({error: err.message});
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    await connectDb();
+    const users = await ExerciseLog.find({}, 'username').exec();
+    res.json(users);  
+  } catch(err) {
+    res.json({error: err.message});
+  }
+});
 
 exports.middleware = app;
